321b320b009484879f4140ca2f62e55e8bebb1b7
Zan Thomson <zan.thomson@gmail.com>
2019-06-19 17:01:15 +1000

    added average of vectors

--- embeddings.py
+++ embeddings.py
@@ -1,5 +1,5 @@
 from bert_embedding import BertEmbedding
-from utils import zomatoPreprocess  
+from bert.utils import zomatoPreprocess  
 
 import numpy as np 
 import pandas as pd
@@ -13,26 +13,56 @@ def stack(embeddings,stacked=[],n=0):
     if len(stacked)>0: x = stacked
     else: x = embeddings[0]
     
-    try:stacked = np.add(np.array(x), embeddings[1])
-    except:return np.array(x)
+    try: stacked = np.add(np.array(x), embeddings[1])
+    except: return np.array(x)
     
     n += 1
     
     return stack(embeddings[1:],stacked=stacked,n=n)
 
 
-def __bagofBERTs(restaurants,urls,berts):
+
+
+def meanVec(embeddings):
+    
+    set_of_means = []
+    dimensionality = embeddings[0].shape[0]
+    
+    n = len(embeddings)
+    mean_vector = []
+
+    for dim in range(dimensionality):
+        cumulative_dimension = 0
+
+        for i in range(n):
+            try:
+                cumulative_dimension += embeddings[i][dim]
+            except:
+                print(i,dim)
+
+        mean_vector += [cumulative_dimension/n]
+    
+    return np.array(mean_vector)
+
+
+
+
+def __bagofBERTs(restaurants,average,urls,berts):
     
     output = []
     x = list(zip(restaurants,urls))
     for (restaurant,url),(cuisines,embeddings) in zip(x,berts):
-        embedding = stack(embeddings)
+        if not(average): 
+            embedding = stack(embeddings)
+        else:
+            embedding = meanVec(embeddings)
+            
         output += [(restaurant,url,cuisines,embedding)]
         
     return output
 
 
-def getEmbeddings(n_restaurants=None,city='sydney',cuisines=True):
+def getEmbeddings(n_restaurants=None,average=True,city='sydney',cuisines=True):
     
     print('Cleaning Zomato data for {}.\n'.format(city))
     cuisines, urls, names = zomatoPreprocess(city.lower(),cuisines=cuisines)
@@ -42,7 +72,7 @@ def getEmbeddings(n_restaurants=None,city='sydney',cuisines=True):
     print('Retrieving BERT sentence representations for {} restuarants...\n'.format(n_restaurants))
     __bert_embedding = BertEmbedding(model='bert_12_768_12')
     __berts = __bert_embedding(cuisines[:n_restaurants])
-    bagofembeddings = __bagofBERTs(names, urls, __berts)
+    bagofembeddings = __bagofBERTs(names, average, urls, __berts)
     
     print('Complete.')
     
